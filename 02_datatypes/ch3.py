8317c53196e2038c1734dab69cfc8f6463906641
Mahipat <mahipatsarvaiya@outlook.com>
2026-06-25 19:33:00 +0530

    add arithmetic operator

---
+++ 02_datatypes/ch3.py
@@ -0,0 +1,36 @@
+#integers
+
+black_tea = 15
+ginger = 3
+total = black_tea + ginger
+print (f"the total is {total}")
+
+
+milk_total = 10
+to_serve = 3
+per_person = milk_total / to_serve
+print(f"serve per person {per_person}")
+
+
+
+milk_total = 10
+to_serve = 3
+per_person = milk_total // to_serve
+print(f"serve per person {per_person}")
+
+
+
+milk_total = 10
+to_serve = 3
+per_person = milk_total % to_serve
+print(f"left for  person {per_person}")
+
+milk_total = 9
+to_serve = 3
+power = milk_total ** to_serve
+print(f"power is {power}")
+
+
+
+total_leaves = 1_000_000
+print(f"total leaves are : {total_leaves}")

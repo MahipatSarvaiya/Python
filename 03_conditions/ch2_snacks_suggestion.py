66720e8a215fa7b760a99eecd83dc1e43c0c2df4
Mahipat <mahipatsarvaiya@outlook.com>
2026-06-29 21:43:22 +0530

    add if else with user input example

---
+++ 03_conditions/ch2_snacks_suggestion.py
@@ -0,0 +1,9 @@
+snack = input("please give order:").lower()
+
+#print(f"your snack is :{snack}")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"your order {snack} is confirmed")
+
+else:
+    print("we don't have that snack, cookies and samosa are available")

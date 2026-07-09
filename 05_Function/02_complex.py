ea5f2fb9408cadb751706cb8ae6571d1eef191f0
Mahipat <mahipatsarvaiya@outlook.com>
2026-07-10 00:04:26 +0530

    Add complex func example

---
+++ 05_Function/02_complex.py
@@ -0,0 +1,17 @@
+def import_data():
+    print("data loading")
+
+def Process_data():
+    print("processing done")
+
+def Filtering_data():
+    print("Filtering done")
+
+def final_report():
+    import_data()
+    Process_data()
+    Filtering_data()
+
+    print("report is ready")
+
+final_report()

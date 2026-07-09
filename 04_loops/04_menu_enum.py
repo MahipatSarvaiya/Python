e7d0ebc896ba6368490e8a9b7512ed51e9dbb8a8
Mahipat <mahipatsarvaiya@outlook.com>
2026-07-09 22:18:31 +0530

    Add loop example

---
+++ 04_loops/04_menu_enum.py
@@ -0,0 +1,8 @@
+menu = ["kitkat","dairy milk","perk","munch","5 star"]
+
+"""
+for item in menu:
+    print(f"menu item is : {item}")
+"""
+for index, item in enumerate(menu, start=1):
+    print(f"menu item {index} is : {item}")

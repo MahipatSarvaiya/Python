0e3046f43d590f45e0c54eae1e289a8e8ace71df
Mahipat <mahipatsarvaiya@outlook.com>
2026-07-10 23:34:40 +0530

    add user registration example

---
+++ 05_Function/03_hiding.py
@@ -0,0 +1,19 @@
+def registrer_user():
+    print("registrer user")
+
+def get_user():
+    print("get user")
+
+def varify_user():
+    print("verify user")
+
+def save_to_db():
+    print("save to db")
+
+def user_registration():
+    registrer_user()
+    get_user()
+    varify_user()
+    save_to_db()
+
+user_registration()

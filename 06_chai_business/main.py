321e69c5d7aeae6b6ccb6dd66299b154970c63b6
Mahipat <mahipatsarvaiya@outlook.com>
2026-07-18 23:34:34 +0530

    add Python import methods

---
+++ 06_chai_business/main.py
@@ -0,0 +1,14 @@
+# import recipes.flavours
+
+# print(recipes.flavours.pink_flavour())
+
+
+# from recipes.flavours import pink_flavour,rose_flavour
+
+# print(pink_flavour())
+# print(rose_flavour())
+
+
+# from ..05_Function.01_duplication import print_order     ..indicated the directories
+
+from .recipes.flavours import rose_flavour
